af0823189a97e7624021046836c61e58e0d28bbd
Curtis Bradley <curtisbradley822@gmail.com>
2026-05-11 00:29:34 -0700

    Blood Pressure Experiment

--- api/tasks.py
+++ api/tasks.py
@@ -23,8 +23,11 @@ class Task:
 
 # Task Definitions
 
-BLOOD_GLUCOSE : Final[Task] = Task('bloodglucose')
+MEASURE_BLOOD_GLUCOSE : Final[Task] = Task('bloodglucose')
 """Measure blood glucose levels"""
+
+MEASURE_BLOOD_PRESSURE : Final[Task] = Task('bloodpressure')
+"""Measure blood pressure levels"""
 
 CHOCOLATE_DARK_40 : Final[Task] = Task('chocdark')
 """Consume 50g of 40% cocoa chocolate"""
@@ -38,6 +41,23 @@ CHOCOLATE_DARK_85 : Final[Task] = Task('chocdark85')
 CHOCOLATE_DARK_99 : Final[Task] = Task('chocdark99')
 """Consume 50g of 99% cocoa chocolate"""
 
+SIT_TEMP_NEG20 : Final[Task] = Task('freezer')
+"""Sit at -20 C for 10 mins"""
+
+SIT_TEMP_40: Final[Task] = Task('heat40')
+"""Sit at 40 C for 10 mins"""
+
+SIT_TEMP_5: Final[Task] = Task('cold')
+"""Sit at 5 C for 10 mins"""
+
+DRINK_WATER_250: Final[Task] = Task('water250')
+"""Drink 250 ml of Water"""
+
+DRINK_WATER_250: Final[Task] = Task('water60')
+"""Drink 60 ml of Water"""
+
+EAT_FRIED_CHIPS : Final[Task] = Task('fries')
+"""Eat 50g of Fried Chips"""
 
 
 __all__ = [name for name in globals() if not name.startswith('_')]
